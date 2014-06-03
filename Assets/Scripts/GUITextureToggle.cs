d4443074f1552dbfcd50c69c2493c65334919f16
jpGrind <therealjessepotter@gmail.com>
2014-06-03 15:06:26 -0400

    Map
    Map

---
+++ Assets/Scripts/GUITextureToggle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUITexture))]
+public class GUITextureToggle : MonoBehaviour {
+
+	public Texture off;
+	public Texture on;
+
+	public void SetState(bool state)
+	{
+		if (state)
+		{
+			guiTexture.texture = on;
+		}
+		else
+		{
+			guiTexture.texture = off;
+		}
+	}
+}
