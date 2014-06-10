7359c77ab550011f8a840e2f414f1966489a217c
Evertras <bfullj@gmail.com>
2014-06-10 12:13:40 -0700

    Better movement selection

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	void Awake()
+	{
+		Update ();
+	}
+	
+	void Update () {
+		transform.LookAt (Camera.main.transform.position);
+	}
+}
